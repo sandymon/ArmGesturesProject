8fef4ea54523bb1f39b308cd86f6a6f7d0508b30
sandymon <rodriguezsandy0306@gmail.com>
2023-06-05 14:04:12 -0700

    Update main.py
    Added wrist movement

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 import cvzone.HandTrackingModule
 import cvzone.SerialModule
 import cv2
-import time;
-cap = cv2.VideoCapture(1)
-detector = cvzone.HandTrackingModule.HandDetector(maxHands=1, detectionCon=0.7)
-mySerial = cvzone.SerialModule.SerialObject(portNo="COM9")
+import time
+
 pTime = 0
 cTime = 0
+cap = cv2.VideoCapture(2)
+detector = cvzone.HandTrackingModule.HandDetector(maxHands=1,detectionCon=0.7)
+serial = cvzone.SerialModule.SerialObject("COM6",9600,1);
 
 while True:
     success, img = cap.read()
-    hands, img = detector.findHands(img)
+    Hands, img = detector.findHands(img)
 
-    if hands:
-        hand1 = hands[0]
-        fingers = detector.fingersUp(hand1)
-        mySerial.sendData(reversed(fingers))
-        #print(fingers)
+    if Hands:
+        myHand = Hands[0]
+        fingerList = detector.fingersUp(myHand)
+        lmList = myHand["lmList"]
+
+        thumbTip = lmList[4]
+        center = lmList[0]
+        if thumbTip[0] > center[0]:
+            fingerList.append(1)
+        else:
+            fingerList.append(0)
+
+
+        #serial.sendData(reversed(fingerList));
+        serial.sendData(fingerList);
+
 
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-    cv2.imshow("Image", img)
 
+    cv2.putText(img, str(int(fps)), (18, 78), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.imshow("Image", img)
     key = cv2.waitKey(1)
 
     if key == ord('q'):
         break
 
+
 cap.release()
-cv2.destroyAllWindows()
+cv2.closeAllWindows
