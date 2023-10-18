047233befb4ad0eeec16cd7cfa9b13eebea9091a
sandymon <rodriguezsandy0306@gmail.com>
2023-10-17 23:26:09 -0400

    added handtrakingModule
    removed the bounding box from hands, this means that only the hand will be dawn  without the box round it,

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import cvzone.HandTrackingModule as htm
+import HandTrackingModule as htm
 import cvzone.SerialModule as srl
 import mediapipe as mp
-import serial as ser
+import serial as serq
 import cv2
 import time
 
@@ -11,8 +11,8 @@ cTime = 0
 cap = cv2.VideoCapture(0)
 
 detector = htm.HandDetector(detectionCon=0.7)
-serial = srl.SerialObject("COM6",9600,1)
-arduino = ser.Serial("", 9600)
+serial = srl.SerialObject("COM6", 9600, 1)
+# arduino = ser.Serial('COM6', 9600)
 
 
 square_size = 225
@@ -25,40 +25,43 @@ text_font = cv2.FONT_HERSHEY_SIMPLEX
 text_size = 1
 text_thickness = 1
 
-timer_durantion = 30
+timer_durantion = 5
 
 
 def draw_hollow_square(image, x, y, size, color, thickness):
     cv2.rectangle(image, (x, y), (x + size, y + size), color, thickness)
 
-def runCamera():
+
+def run_camera():
     start_time = time.time()
-    while start_time <= timer_durantion:
+    while time.time() - start_time < timer_durantion:
+        print(start_time)
         success, image = cap.read()
         Hands, image = detector.findHands(image, draw=False)
-
 
         imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         draw_hollow_square(image, square_x, square_y, square_size, square_color, square_thickness)
         draw_hollow_square(image, square_x * 4, square_y, square_size, square_color, square_thickness)
         cv2.putText(image, "Right hand", (square_x, square_y + 220), text_font, text_size, text_color, text_thickness)
-        cv2.putText(image, "Left hand", (square_x * 4, square_y + 220), text_font, text_size, text_color, text_thickness)
+        cv2.putText(image, "Left hand", (square_x * 4, square_y + 220), text_font, text_size, text_color,
+                    text_thickness)
 
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(image, str(int(fps)), (18, 78), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        # cTime = start_time
+        # fps = 1 / (cTime - pTime)
+        # pTime = cTime
+        #
+        # cv2.putText(image, str(int(fps)), (18, 78), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
         # Loop through detected hands
         for hand in Hands:
             lmList = hand["lmList"]
-            if square_x <= lmList[8][0] <= square_x + square_size and square_y <= lmList[8][1] <= square_y + square_size:
+            if square_x <= lmList[8][0] <= square_x + square_size and square_y <= lmList[8][
+                1] <= square_y + square_size:
                 # Draw landmarks or process the hand here
                 Hands, image = detector.findHands(image)
 
-                #finger Data sent to arduino
+                # finger Data sent to arduino
                 if Hands:
                     myHand = Hands[0]
                     fingerList = detector.fingersUp(myHand)
@@ -80,11 +83,10 @@ def runCamera():
         cv2.waitKey(1)
 
 
-
 if __name__ == '__main__':
-    while True:
+    # while True:
         # Read data from the Arduino
-        data = arduino.readline().decode().strip()
-        serial.getData()
-        if data == "ButtonPressed":
-            runCamera()
+        # data = arduino.readline().decode().strip()
+        # serial.getData()
+        # if data == "ButtonPressed":
+        run_camera()
