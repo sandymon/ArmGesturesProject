31c9fa12a15b2015f16126ed2f9df4e7a9649efa
sandymon <rodriguezsandy0306@gmail.com>
2023-10-17 23:34:38 -0400

    cleaned up the code

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
+import time
+import cv2
 import HandTrackingModule as htm
 import cvzone.SerialModule as srl
-import mediapipe as mp
-import serial as serq
-import cv2
-import time
 
-pTime = 0
-cTime = 0
 # Initialize OpenCV and MediaPipe Hands
 cap = cv2.VideoCapture(0)
 
 detector = htm.HandDetector(detectionCon=0.7)
 serial = srl.SerialObject("COM6", 9600, 1)
-# arduino = ser.Serial('COM6', 9600)
-
 
 square_size = 225
 square_x = 100
@@ -25,43 +19,29 @@ text_font = cv2.FONT_HERSHEY_SIMPLEX
 text_size = 1
 text_thickness = 1
 
-timer_durantion = 5
-
+timer_duration = 15
 
 def draw_hollow_square(image, x, y, size, color, thickness):
     cv2.rectangle(image, (x, y), (x + size, y + size), color, thickness)
 
-
 def run_camera():
     start_time = time.time()
-    while time.time() - start_time < timer_durantion:
-        print(start_time)
+    while time.time() - start_time < timer_duration:
         success, image = cap.read()
-        Hands, image = detector.findHands(image, draw=False)
 
+        Hands, image = detector.findHands(image, draw=False)
         imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         draw_hollow_square(image, square_x, square_y, square_size, square_color, square_thickness)
         draw_hollow_square(image, square_x * 4, square_y, square_size, square_color, square_thickness)
+
         cv2.putText(image, "Right hand", (square_x, square_y + 220), text_font, text_size, text_color, text_thickness)
-        cv2.putText(image, "Left hand", (square_x * 4, square_y + 220), text_font, text_size, text_color,
-                    text_thickness)
+        cv2.putText(image, "Left hand", (square_x * 4, square_y + 220), text_font, text_size, text_color, text_thickness)
 
-        # cTime = start_time
-        # fps = 1 / (cTime - pTime)
-        # pTime = cTime
-        #
-        # cv2.putText(image, str(int(fps)), (18, 78), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-        # Loop through detected hands
         for hand in Hands:
             lmList = hand["lmList"]
-            if square_x <= lmList[8][0] <= square_x + square_size and square_y <= lmList[8][
-                1] <= square_y + square_size:
-                # Draw landmarks or process the hand here
+            if square_x <= lmList[8][0] <= square_x + square_size and square_y <= lmList[8][1] <= square_y + square_size:
                 Hands, image = detector.findHands(image)
-
-                # finger Data sent to arduino
                 if Hands:
                     myHand = Hands[0]
                     fingerList = detector.fingersUp(myHand)
@@ -74,19 +54,17 @@ def run_camera():
                     else:
                         fingerList.append(0)
 
-                    # serial.sendData(reversed(fingerList));
                     serial.sendData(fingerList)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
         cv2.imshow("Output", image)
         cv2.waitKey(1)
 
 
+# TODO: implement somthing to read the arduino data so
+#  that we can run run_camera() once we press a button.
 if __name__ == '__main__':
-    # while True:
-        # Read data from the Arduino
-        # data = arduino.readline().decode().strip()
-        # serial.getData()
-        # if data == "ButtonPressed":
-        run_camera()
+    run_camera()
+
+# Release the camera and close OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
